fcd02f1fb9eb40028211c4343ffa3d3a35dec68e
JaneBee24 <bublikkjenya@gmail.com>
2023-10-12 00:10:33 +0200

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,5 @@
-import basicLightbox from 'https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.esm.min.js';
+
+import basicLightbox from 'https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.js';
 
 const galleryItems = [
   {
